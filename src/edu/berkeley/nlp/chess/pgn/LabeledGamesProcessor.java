6d393fca71fa1c51610c06dc38ad54348af6a69e
Richard Shin <ricshin@berkeley.edu>
2011-05-16 20:52:43 -0700

    Update LabeledGamesProcessor to save results to a serialized file.

--- src/edu/berkeley/nlp/chess/pgn/LabeledGamesProcessor.java
+++ src/edu/berkeley/nlp/chess/pgn/LabeledGamesProcessor.java
@@ -2,85 +2,101 @@ package edu.berkeley.nlp.chess.pgn;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.ObjectOutputStream;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.zip.GZIPOutputStream;
 
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Multimap;
+import org.apache.commons.io.filefilter.FileFilterUtils;
 
 import chesspresso.game.Game;
 import chesspresso.pgn.PGNReader;
 import chesspresso.pgn.PGNSyntaxError;
-import chesspresso.pgn.PGNWriter;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.io.Files;
 
 import edu.berkeley.nlp.chess.Games;
 import edu.berkeley.nlp.chess.PositionWithMoves;
 import edu.berkeley.nlp.chess.util.FileIterator;
+import edu.berkeley.nlp.util.CommandLineUtils;
 
 public class LabeledGamesProcessor {
 	public static String getTag(String filename) {
 		return filename.substring(filename.lastIndexOf('/') + 1, filename.lastIndexOf('.'));
 	}
-	public static void main(String[] args) {
-		long games = 0, largegames = 0;
-		
+	
+	public static void main(String[] args) throws IOException {
 		assert getTag("themes/t/deflection.pgn-fixed").equals("deflection") : "getTag doesn't work";
-		
-		PrintWriter pw_out = new PrintWriter(System.out);
-		Multimap<List<PositionWithMoves>, String> tags = HashMultimap.create();
 
+		long games = 0, largeGames = 0;
+		HashMultimap<List<PositionWithMoves>, String> tags = HashMultimap.create();
 		int[] counts = new int[10];
-		for (File f : new FileIterator(args[0])) {
+		
+		Map<String, String> argMap = CommandLineUtils.simpleCommandLineParser(args);
+		int maxLength = Integer.parseInt(argMap.get("-maxLength"));
+		String prefix = argMap.get("-prefix");
+		String outputPath = argMap.get("-output");
+		int sliceLength = Integer.parseInt(argMap.get("-sliceLength"));
+		
+		for (File f : new FileIterator(new File(prefix), FileFilterUtils.suffixFileFilter(".pgn-fixed"))) {
 			String filename = f.getPath();
-			
-			if (filename.endsWith("-fixed")) {
-				try {
-					PGNReader reader = new PGNReader(filename);
-					Game g;
-	
-					int linenumber = 0;
-					System.out.printf("Parsing %s\n", filename);
-					while (true) {
-						try {
-							g = reader.parseGame();
-	
-							if (g == null) {
-								break;
-							}
-							
-							linenumber = reader.getLineNumber();
+			PGNReader reader = new PGNReader(filename);
+			Game g;
 
-							List<PositionWithMoves> boards = Games.flatten(g);
-							if (boards.size() > 10) {
-								System.out.printf("\tThrew out large game (%d): %s line %d\n", boards.size(), filename, linenumber);
-								++largegames;
-								continue;
-							}
-							counts[boards.size() - 1]++;
-							for (int i = 1; i < boards.size(); ++i) {
-								tags.put(boards.subList(0, i), getTag(filename));
-							}
-	
-							++games;
-						} catch (PGNSyntaxError e) {
-							System.out.printf("\n\tPGNSyntaxError (line %d of good game; error line %d): %s", linenumber, reader.getLineNumber(), e.getMessage());
-							// e.printStackTrace(System.out);
-						} catch (RuntimeException e) {
-							// System.out.printf("\n\tRuntime (line %d of good game; error line %d): %s", linenumber, reader.getLineNumber(), e.getMessage());
-							// e.printStackTrace(System.out);
-						}
+			int lineNumber = 0;
+			System.out.printf("Parsing %s\n", filename);
+			while (true) {
+				try {
+					g = reader.parseGame();
+					if (g == null) break;
+
+					lineNumber = reader.getLineNumber();
+					List<PositionWithMoves> boards = Games.flatten(g);
+					if (boards.size() > maxLength) {
+						System.out.printf("\tThrew out large game (%d): %s line %d\n", boards.size(), filename, lineNumber);
+						++largeGames;
+						continue;
 					}
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					counts[boards.size() - 1]++;
+					for (int i = 1; i <= sliceLength && i <= boards.size(); ++i) {
+						tags.put(PositionWithMoves.getPrefix(boards, i), getTag(filename));
+					}
+
+					++games;
+				} catch (PGNSyntaxError e) {
+					System.out.printf("\n\tPGNSyntaxError (line %d of good game; error line %d): %s", lineNumber, reader.getLineNumber(), e.getMessage());
+					// e.printStackTrace(System.out);
+				} catch (RuntimeException e) {
+					// System.out.printf("\n\tRuntime (line %d of good game; error line %d): %s", linenumber, reader.getLineNumber(), e.getMessage());
+					e.printStackTrace(System.out);
 				}
 			}
-			pw_out.flush();
 		}
+		// Invert the map
+		Multimap<String, List<PositionWithMoves>> slicesByTag = HashMultimap.create();
+		for (Entry<List<PositionWithMoves>, String> entry : tags.entries())
+			slicesByTag.put(entry.getValue(), entry.getKey());
 		
-		System.out.printf("Parsed %d games, added %d board-tag configurations, threw out %d large games\n", games, tags.size(), largegames);
-		for (int i = 0; i < counts.length; ++i) {
-			System.out.printf("%3d: %d\n", i, counts[i]);
-		}
+		Preconditions.checkState(slicesByTag.size() == tags.size());
+		System.out.printf("Parsed %d games, added %d board-tag configurations (with %d unique tags), threw out %d large games\n", games, tags.size(), slicesByTag.keySet().size(), largeGames);
+		
+		ObjectOutputStream oos = new ObjectOutputStream(
+				new GZIPOutputStream(
+						Files.newOutputStreamSupplier(new File(outputPath + ".features.gz")).getOutput()));
+		oos.writeObject(slicesByTag);
+		oos.close();
+		System.out.println("Saved to " + outputPath + ".features.gz.");
+//		for (List<PositionWithMoves> lpwm : tags.keySet()) {
+//			if (tags.get(lpwm).size() > 1)
+//			System.out.printf("%s: %s\n", lpwm, Joiner.on(", ").join(tags.get(lpwm)));
+//		}
+		
+//		for (int i = 0; i < counts.length; ++i) {
+//			System.out.printf("%3d: %d\n", i, counts[i]);
+//		}
 	}
 }
